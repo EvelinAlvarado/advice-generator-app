2151f3ec6e73dffdc4f31c21625ee845ed9bc78b
Evelin Alvarado <129849677+EvelinAlvarado@users.noreply.github.com>
2024-05-15 12:27:09 +0100

    feat: Create main App component with advice card and footer
    - Implemented the main App component.
    - Added an advice card with a dynamic advice ID and text.
    - Included a divider image and a dice icon button with hover effects.
    - Created a footer with attribution and social media links.
    - Applied appropriate styling using Tailwind CSS.

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,59 @@
 function App() {
   return (
     <>
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+      <article className="bg-Dark-Grayish-Blue w-[342px] rounded-xl px-[30px] pt-10 pb-16 text-center relative ">
+        <h2 className="text-Neon-Green uppercase mb-6 tracking-[4px] text-xs">
+          Advice #<span id="adviceId">117</span>
+        </h2>
+        <p
+          className="text-Light-Cyan text-[22px] font-extrabold mb-6"
+          id="advice"
+        >
+          "Lorem ipsum dolor sit amet consectetur adipisicing elit. Delectus
+          atque facere, omnis repellendust."
+        </p>
+        <img
+          src="../src/assets/images/pattern-divider-mobile.svg"
+          alt="divider"
+        />
+        <div className="bg-Neon-Green w-[66px] h-[66px] rounded-full grid  place-content-center absolute bottom-[-33px] left-[138px] cursor-pointer hover:drop-shadow-[0_0_16px_hsl(150,100%,66%)] ">
+          <img src="../src/assets/images/icon-dice.svg" alt="dice" />
+        </div>
+      </article>
+
+      <footer className="text-Light-Cyan absolute top-[90%] text-xs left-[21%] md:left-[35%] lg:left-[42%] text-center tracking-[2px] ">
+        <p>
+          Challenge by{" "}
+          <a href="https://www.frontendmentor.io?ref=challenge" target="_blank">
+            Frontend Mentor
+          </a>
+          .
+        </p>
+        <p>
+          Coded by{" "}
+          <strong className="hover:drop-shadow-[0_0_16px_hsl(150,100%,66%)]">
+            <a href="#">Evelin</a>
+          </strong>{" "}
+          .
+        </p>
+
+        <div className="flex justify-center gap-2 tracking-tight">
+          <a
+            href="https://github.com/EvelinAlvarado"
+            target="_blank"
+            className="w-4 hover:drop-shadow-[0_0_16px_hsl(150,100%,66%)]"
+          >
+            <img src="github.svg" alt="GitHub" className="" />
+          </a>
+          <a
+            href="https://www.linkedin.com/in/evelinalvarado/"
+            target="_blank"
+            className="w-4 hover:drop-shadow-[0_0_16px_hsl(150,100%,66%)]"
+          >
+            <img src="linkedin.svg" alt="LinkedIn" />
+          </a>
+        </div>
+      </footer>
     </>
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,21 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        /* Primary */
+        "Light-Cyan": "hsl(193, 38%, 86%)",
+        "Neon-Green": "hsl(150, 100%, 66%)",
+
+        /* Neutral */
+        "Grayish-Blue": "hsl(217, 19%, 38%)",
+        "Dark-Grayish-Blue": "hsl(217, 19%, 24%)",
+        "Dark-Blue": "hsl(218, 23%, 16%)",
+      },
+      fontFamily: {
+        Manrope: ["Manrope", "sans-serif"],
+      },
+    },
   },
   plugins: [],
 };
