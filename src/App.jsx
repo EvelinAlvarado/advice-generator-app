2d5a0a10e24132611ff047afbdc82ab03f792ffd
Evelin Alvarado <129849677+EvelinAlvarado@users.noreply.github.com>
2024-05-16 10:47:08 +0100

    Add API advice

--- src/App.jsx
+++ src/App.jsx
@@ -1,39 +1,82 @@
+import mobileDivider from "./assets/images/pattern-divider-mobile.svg";
+import desktopDivider from "./assets/images/pattern-divider-desktop.svg";
+import iconDice from "./assets/images/icon-dice.svg";
+import { useState } from "react";
+
+/* Client Service */
+const getAdvice = async () => {
+  try {
+    // Call API
+    const response = await fetch(`https://api.adviceslip.com/advice`);
+    return response.json();
+  } catch (error) {
+    throw new Error("Error fetching advice: ", error.message);
+  }
+};
+
 function App() {
+  const [slipId, setSlipId] = useState("0");
+  const [advice, setAdvice] = useState("Click the dice to get an advice");
+
+  const handleClick = async () => {
+    // console.log("Dice was clicked");
+    const data = await getAdvice();
+    setSlipId(data.slip.id);
+    setAdvice(data.slip.advice);
+    // console.log(data);
+    // console.log(data.slip);
+    // console.log(data.slip.id);
+    // console.log(data.slip.advice);
+  };
   return (
     <>
-      <article className="bg-Dark-Grayish-Blue w-[342px] rounded-xl px-[30px] pt-10 pb-16 text-center relative ">
+      <article className="bg-Dark-Grayish-Blue w-[342px] rounded-xl px-[30px] pt-10 pb-16 text-center relative md:w-[540px]">
         <h2 className="text-Neon-Green uppercase mb-6 tracking-[4px] text-xs">
-          Advice #<span id="adviceId">117</span>
+          Advice #<span id="adviceId">{slipId}</span>
         </h2>
         <p
           className="text-Light-Cyan text-[22px] font-extrabold mb-6"
           id="advice"
         >
-          "Lorem ipsum dolor sit amet consectetur adipisicing elit. Delectus
-          atque facere, omnis repellendust."
+          "{advice}"
         </p>
-        <img
-          src="../src/assets/images/pattern-divider-mobile.svg"
-          alt="divider"
-        />
-        <div className="bg-Neon-Green w-[66px] h-[66px] rounded-full grid  place-content-center absolute bottom-[-33px] left-[138px] cursor-pointer hover:drop-shadow-[0_0_16px_hsl(150,100%,66%)] ">
-          <img src="../src/assets/images/icon-dice.svg" alt="dice" />
+        <picture>
+          <source media="(max-width:640px)" srcSet={mobileDivider} />
+          <source media="(min-width:64px)" srcSet={desktopDivider} />
+          {/* Etiquetas <picture> y <source>: Usadas para especificar diferentes versiones de la imagen basadas en condiciones como el tamaño de la pantalla.
+          Etiqueta <img> con src: Actúa como una imagen de fallback para navegadores que no soportan <picture> y <source>.
+          Importancia: Garantiza que una imagen se mostrará incluso si el navegador no soporta <picture> y <source>. */}
+          <img src={desktopDivider} alt="divider" className="mx-auto" />
+        </picture>
+
+        <div
+          onClick={handleClick}
+          className="bg-Neon-Green w-[66px] h-[66px] rounded-full grid  place-content-center absolute bottom-[-33px] left-[138px] cursor-pointer hover:drop-shadow-[0_0_16px_hsl(150,100%,66%)] md:left-[237px] "
+        >
+          <img src={iconDice} alt="dice" />
         </div>
       </article>
 
-      <footer className="text-Light-Cyan absolute top-[90%] text-xs left-[21%] md:left-[35%] lg:left-[42%] text-center tracking-[2px] ">
+      <footer className="text-Light-Cyan mt-40 text-center gap-1 ">
         <p>
           Challenge by{" "}
-          <a href="https://www.frontendmentor.io?ref=challenge" target="_blank">
+          <a
+            href="https://www.frontendmentor.io?ref=challenge"
+            className="text-Neon-Green"
+            target="_blank"
+          >
             Frontend Mentor
           </a>
           .
         </p>
-        <p>
+        <p className="mb-1">
           Coded by{" "}
-          <strong className="hover:drop-shadow-[0_0_16px_hsl(150,100%,66%)]">
-            <a href="#">Evelin</a>
-          </strong>{" "}
+          <a
+            href="#"
+            className="hover:drop-shadow-[0_0_16px_hsl(150,100%,66%)] text-Neon-Green"
+          >
+            Evelin
+          </a>
           .
         </p>
 
@@ -41,14 +84,14 @@ function App() {
           <a
             href="https://github.com/EvelinAlvarado"
             target="_blank"
-            className="w-4 hover:drop-shadow-[0_0_16px_hsl(150,100%,66%)]"
+            className="w-6 hover:drop-shadow-[0_0_16px_hsl(150,100%,66%)]"
           >
             <img src="github.svg" alt="GitHub" className="" />
           </a>
           <a
             href="https://www.linkedin.com/in/evelinalvarado/"
             target="_blank"
-            className="w-4 hover:drop-shadow-[0_0_16px_hsl(150,100%,66%)]"
+            className="w-6 hover:drop-shadow-[0_0_16px_hsl(150,100%,66%)]"
           >
             <img src="linkedin.svg" alt="LinkedIn" />
           </a>
